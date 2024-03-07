0305442ef636dca20945045ff2391f538fee7c6b
atsu <atsgula@gmail.com>
2024-03-07 14:40:08 -0500

    fix api routes package

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,6 +1,7 @@
 import { Router } from 'express';
 import { createSession, getResponse } from '../app/Http/Controllers/payment.controller.js';
 import productController from '../app/Http/Controllers/product.controller.js';
+import packageController from '../app/Http/Controllers/package.controller.js';
 
 const router = Router();
 
@@ -12,9 +13,16 @@ router.get('/cancel', (req, res) => res.send('cancel'))
 
 // Products
 router.get('/products', productController.all)
-router.get('/products/:id', productController.one)
+router.get('/products/show/:id', productController.one)
 router.post('/products/create', productController.create)
-router.put('/products/:id', productController.update)
+router.put('/products/edit/:id', productController.update)
 router.delete('/products/:id', productController.delete)
+
+// Packages
+router.get('/packages', packageController.all)
+router.get('/packages/show/:id', packageController.one)
+router.post('/packages/create', packageController.create)
+router.put('/packages/edit/:id', packageController.update)
+router.delete('/packages/:id', packageController.delete)
 
 export default router;
