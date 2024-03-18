b2281b58612bc15c04e10592329adbed06279bf5
William sanchez <williamlopez0721@gmail.com>
2024-03-18 16:03:49 -0500

    new changes in the view home, implement navbar and header section

--- src/app/components/navbar/Navbar.jsx
+++ src/app/components/navbar/Navbar.jsx
@@ -1,9 +1,20 @@
-const Navbar = ()=> {
-    return(
-        <nav>
-        
-        </nav>
-    )
-}
+import React from 'react';
+import styles from './Navbar.module.css'; 
+
+const Navbar = () => {
+  return (
+    <nav className={styles.navbar}> 
+        <div>
+            <img src="logo.svg" alt="" width={170} />
+        </div>
+      <ul className={styles['navbar-list']}> 
+        <li className={styles['navbar-item']}>Home</li>
+        <li className={styles['navbar-item']}>Products</li> 
+        <li className={styles['navbar-item']}>Benefits</li> 
+        <li className={styles['navbar-item']}>Packages</li> 
+      </ul>
+    </nav>
+  );
+};
 
 export default Navbar;
