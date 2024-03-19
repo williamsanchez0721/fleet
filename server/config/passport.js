2df09441b82e04392da2872c52aaf4de062f5316
atsu <atsgula@gmail.com>
2024-03-18 22:40:04 -0500

    fix demo

---
+++ server/config/passport.js
@@ -0,0 +1,26 @@
+import { Strategy as GoogleStrategy } from 'passport-google-oauth20';
+import passport from 'passport';
+
+passport.use(
+	new GoogleStrategy(
+		{
+			clientID: process.env.CLIENT_ID,
+			clientSecret: process.env.CLIENT_SECRET,
+			callbackURL: process.env.CALLBACKURL,
+			scope: ["profile", "email"],
+		},
+		function (accessToken, refreshToken, profile, callback) {
+			console.log('Profile Data')
+			console.log(profile)
+			callback(null, profile);
+		}
+	)
+);
+
+passport.serializeUser((user, done) => {
+	done(null, user);
+});
+
+passport.deserializeUser((user, done) => {
+	done(null, user);
+});
