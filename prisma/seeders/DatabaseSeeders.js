d76608bb4a783c2615eaa437ad7fd7b1a0b42aaf
atsu <atsgula@gmail.com>
2024-03-25 22:48:27 -0500

    add seeder

--- prisma/seeders/DatabaseSeeders.js
+++ prisma/seeders/DatabaseSeeders.js
@@ -1,11 +1,17 @@
 
 import { PrismaClient } from '@prisma/client';
 import RoleSeeder from './RoleSeeder.js';
+import UserSeeder from './UserSeeder.js';
+import SubscriptionSeeder from './SubscriptionSeeder.js';
+import ProductSeeder from './ProductSeeder.js';
 
 const prisma = new PrismaClient();
 
 async function main() {
   await RoleSeeder(prisma);
+  await UserSeeder(prisma);
+  await SubscriptionSeeder(prisma);
+  await ProductSeeder(prisma);
 }
 
 main()
@@ -13,5 +19,6 @@ main()
     throw e;
   })
   .finally(async () => {
+    console.log("Seeders executed successfully.");
     await prisma.$disconnect();
   });
